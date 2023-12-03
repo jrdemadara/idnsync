1ca66f3a269fc4d1018f5321067a43ffb7133733
Johnny Roger <jrdemadara@protonmail.com>
2023-12-03 22:22:51 +0800

    added check update

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,8 @@
-import { app, shell, BrowserWindow } from 'electron'
+import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import sql from 'mssql'
+import fs from 'fs'
+import path from 'path'
+
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
@@ -69,3 +73,69 @@ app.on('window-all-closed', () => {
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
+
+// Listen for messages from the renderer process to update settings
+ipcMain.on('update-settings', async (event, newSettings) => {
+  try {
+    // Read the existing settings
+    const settingsPath = path.join(app.getPath('userData'), 'settings.json')
+    const existingSettingsData = await fs.readFile(settingsPath, 'utf-8')
+    const existingSettings = JSON.parse(existingSettingsData)
+
+    // Update the database configuration
+    existingSettings.database = newSettings.database
+
+    // Write the updated settings back to the file
+    await fs.writeFile(settingsPath, JSON.stringify(existingSettings, null, 2))
+
+    // Respond to the renderer process with a success message or updated settings
+    event.reply('update-settings-response', { success: true, settings: existingSettings })
+  } catch (error) {
+    // Handle errors updating the settings
+    console.error('Error updating settings:', error)
+
+    // Respond to the renderer process with an error message
+    event.reply('update-settings-response', { success: false, error: error.message })
+  }
+})
+
+// Listen for messages from the renderer process to insert data
+ipcMain.on('insert-data', async (event, dataToInsert) => {
+  try {
+    // Read the settings file
+    const settingsPath = path.join(app.getPath('userData'), 'settings.json')
+    const settingsData = await fs.readFile(settingsPath, 'utf-8')
+    const settings = JSON.parse(settingsData)
+
+    // Configure the default database connection
+    await sql.connect(settings.database)
+    // Configure your MSSQL connection
+    const config = {
+      user: 'your_username',
+      password: 'your_password',
+      server: 'your_server',
+      database: 'your_database',
+      options: {
+        encrypt: true // Use this if you're on Windows Azure
+      }
+    }
+
+    // Connect to the database
+    await sql.connect(config)
+
+    // Perform the database operation (insert data)
+    const result = await sql.query`INSERT INTO your_table (column_name) VALUES (${dataToInsert})`
+
+    // Handle success or send a response back to the renderer process
+    event.reply('insert-data-response', { success: true, result })
+  } catch (error) {
+    // Handle errors
+    console.error('Error inserting data:', error)
+
+    // Send an error response back to the renderer process
+    event.reply('insert-data-response', { success: false, error: error.message })
+  } finally {
+    // Close the database connection
+    await sql.close()
+  }
+})
--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,35 +1,102 @@
 <script setup>
+import { ref } from 'vue'
 import axios from 'axios'
+// import logo from './assets/icons.svg'
+import { Settings, HelpCircle } from 'lucide-vue-next'
+
+const loading = ref(false)
+const status = ref('Synchronize')
+const message = ref('Click to synchronize data.')
 
 const syncData = async () => {
-  axios
-    .get('https://node1.jrdemadara.dev/api/v1/uri/pull_update', {
-    })
-    .then(function (response) {
-      console.log(response)
-    })
-    .catch(function (error) {
-      console.log(error)
+  if (!loading.value) {
+    loading.value = true
+    status.value = 'Checking'
+    message.value = 'Checking for update...'
 
-    })
-    .finally(function () {
+    await new Promise((resolve) => setTimeout(resolve, 5000))
 
-    })
+    axios
+      .get('http://127.0.0.1:20230/api/v1/uri/check_update', {
+        headers: {
+          authorization:
+            'softspark@zgmEl=Z4Bqdm402RuNJg17RAP9kGKYq8SbrNNlRdnygJws!n5XE1Ob=mmElEQLUjYOEiYDUkPJtFHccFuaLrt9u6uCPIonOfrq/4MqojK!vOgIp4CSS8aj?0um7fH4jcWOCBWZFkn844WGuySOxY-Hkkj0P-AwXOi7pMjP!PJ3DBLUZi6LTtio/MY?BWbynV8HTPGAoLBqvzu0RC6TXGIFvzw?klBAw0d-dut6Ks3SNGKZtAqfSB1P'
+        }
+      })
+      .then(function (response) {
+        if (response.data.count > 0) {
+          status.value = 'Synchronizing'
+          message.value = `Synchronizing ${response.data.count} data...`
+          console.log(response.data.count)
+          //Save data to sql
+        } else {
+          loading.value = false
+          status.value = 'Resync'
+          message.value = 'Database is up to date.'
+          console.log(response.data.count)
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+      .finally(function () {})
+  }
 }
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center w-screen h-screen bg-slate-950">
-    <div class="relative flex justify-center items-center w-64 h-64 rounded-full">
-      <span class="animate-ping absolute inline-flex w-40 h-40 rounded-full bg-violet-700"></span>
-      <div class="absolute">
-        <button type="button"
-          class="w-64 h-64 rounded-full shadow-inner border-8 border-violet-900 bg-violet-700 hover:bg-violet-600 hover:border-violet-800 hover:transition hover:duration-300 hover:ease-in-out active:bg-violet-700 text-3xl text-slate-50">
-          Sync
-        </button>
+  <div class="flex flex-col w-screen h-screen bg-slate-950">
+    <header class="flex justify-between w-screen h-fit p-4 bg-transparent">
+      <div class="flex justify-center items-center">
+        <img src="./assets/icon.png" alt="logo" class="w-10 mr-2" />
+        <h4 class="font-bold text-slate-50 mr-1">
+          IDNSync <span class="font-light text-xs font-mono">v1.0</span>
+        </h4>
       </div>
+      <div class="flex">
+        <HelpCircle :size="24" :stroke-width="1" class="text-slate-50" />
+        <Settings :size="24" :stroke-width="1" class="ml-3 text-slate-50" />
+      </div>
+    </header>
+    <div class="flex flex-col justify-center items-center w-full h-full">
+      <div class="relative flex justify-center items-center w-64 h-64 rounded-full">
+        <span
+          class="absolute inline-flex w-48 h-48 rounded-full"
+          :class="{
+            'animate-ping': loading,
+            'bg-orange-700': status == 'Checking',
+            'bg-rose-700': status == 'Synchronizing'
+          }"
+        ></span>
+        <div class="absolute">
+          <button
+            type="button"
+            :class="{
+              'bg-violet-700 border-violet-500 hover:bg-violet-600 hover:border-violet-400':
+                status == 'Synchronize',
+              'bg-orange-700 border-orange-500': status == 'Checking',
+              'bg-rose-700 border-rose-500': status == 'Synchronizing',
+              'bg-blue-700 border-blue-500 hover:bg-blue-600 hover:border-blue-400':
+                status == 'Resync',
+              'bg-green-700 border-green-500 hover:bg-green-600 hover:border-green-400':
+                status == 'Complete'
+            }"
+            class="w-64 h-64 rounded-full shadow-inner border-8 hover:transition hover:duration-300 hover:ease-in-out text-2xl text-slate-50"
+            @click="syncData"
+          >
+            {{ status }}
+          </button>
+        </div>
+      </div>
+      <p
+        class="text-xl text-slate-50 mt-5"
+        :class="{
+          'animate-pulse': message !== 'Click to synchronize data.'
+        }"
+      >
+        {{ message }}
+      </p>
     </div>
-    <p class="text-xl text-slate-50 mt-5">Please wait...</p>
   </div>
 </template>
 
